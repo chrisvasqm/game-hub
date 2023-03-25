34ba0460ddb760dc8805a6a33759adf2a1120813
chrisvasqm <christianvasquezmelo@gmail.com>
2023-03-25 19:44:41 -0400

    Add search filtering

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,32 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { useRef } from 'react';
 import { BsSearch } from 'react-icons/bs';
 
-function SearchInput() {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+function SearchInput({ onSearch }: Props) {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input placeholder='Search for games...' borderRadius={20} variant='filled' />
-    </InputGroup>
+    <form
+      className='q'
+      onSubmit={event => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          placeholder='Search for games...'
+          borderRadius={20}
+          variant='filled'
+        />
+      </InputGroup>
+    </form>
   );
 }
 
