97fc2a107ccdf5d15d1a6b57cd2ac945e0d95c16
chrisvasqm <christianvasquezmelo@gmail.com>
2023-03-25 01:42:36 -0400

    Update skeleton image height

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import { Card, Skeleton, SkeletonText, CardBody } from '@chakra-ui/react';
 function GameCardSkeleton() {
   return (
     <Card width='300px' borderRadius={10} boxShadow='2xl' overflow='hidden'>
-      <Skeleton height='220px' />
+      <Skeleton height='200px' />
       <CardBody>
         <SkeletonText />
       </CardBody>
