b8a23fb8ee0e2dc2dac622c9b3d573205233cbfa
chrisvasqm <christianvasquezmelo@gmail.com>
2024-03-16 12:01:04 -0400

    Add intial platform data

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,13 +2,16 @@ import { useQuery } from '@tanstack/react-query';
 import { FetchDataResponse } from './useData';
 import { Platform } from './useGames';
 import apiClient from '../services/api-client';
+import platforms from '../data/platforms';
 
 const usePlatforms = () => useQuery({
     queryKey: ['platforms'],
     queryFn: () =>
         apiClient
             .get<FetchDataResponse<Platform>>('/platforms/lists/parents')
-            .then(response => response.data)
+            .then(response => response.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: platforms.length, results: platforms }
 })
 
 export default usePlatforms;
