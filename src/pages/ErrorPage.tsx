e78cc10b20d9e3759b40bfeb3505a94aa31d7ed4
chrisvasqm <christianvasquezmelo@gmail.com>
2024-03-21 14:17:57 -0400

    Add routing error page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,19 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+function ErrorPage() {
+    const error = useRouteError();
+    return (
+        <>
+            <NavBar />
+            <Box padding={4}>
+                <Heading>Oops!</Heading>
+                {isRouteErrorResponse(error) ?
+                    <Text>An error ocurred</Text> : <Text>Invalid page</Text>}
+            </Box>
+        </>
+    )
+}
+
+export default ErrorPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 import GameDetailsPage from "./pages/GameDetailsPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 
 const router = createBrowserRouter([
-    { 
-        path: '/', 
+    {
+        path: '/',
         element: <Layout />,
+        errorElement: <ErrorPage />,
         children: [
-            { path: '', element: <HomePage />},
-            { path: 'games/:id', element: <GameDetailsPage />}
+            { path: '', element: <HomePage /> },
+            { path: 'games/:id', element: <GameDetailsPage /> }
         ]
     },
 ]);
